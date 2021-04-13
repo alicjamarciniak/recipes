d7b09bc47ef0538b1817121a5385b4022b8c6e85
alicjamarciniak <marciniak.al@gmail.com>
2021-04-13 03:15:44 +0100

    Adds navbar logo and adds style rules to navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,57 @@
-import { AppBar, Toolbar, IconButton, Typography, makeStyles } from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu'
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import logo from './../../images/logo.png';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1
+  header: {
+    flexGrow: 1,
+  },
+  appBar: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
   },
   menuButton: {
-    marginRight: theme.spacing(2)
+    marginRight: theme.spacing(4),
   },
   title: {
     flexGrow: 1,
-    fontSize: 36
-  }
-}))
+    fontSize: 25,
+  },
+  logo: {
+    maxWidth: 60,
+    marginRight: theme.spacing(2),
+  },
+}));
 
 const Header = () => {
-  const muiClasses = useStyles()
+  const muiClasses = useStyles();
 
   return (
-    <header className={muiClasses.root}>
-      <AppBar className="Header" position="static">
+    <header className={muiClasses.header}>
+      <AppBar className={muiClasses.appBar} position="sticky">
         <Toolbar>
-          <IconButton edge="start" className={muiClasses.menuButton} color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
+          <IconButton
+            edge="start"
+            className={muiClasses.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <img src={logo} alt="My recipes" className={muiClasses.logo} />
           <Typography variant="h1" className={`title ${muiClasses.title}`}>
             My recipes
           </Typography>
         </Toolbar>
       </AppBar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
